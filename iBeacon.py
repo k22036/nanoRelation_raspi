dd929155e5103faaf5e9a1842ea140b0c438b65b
k22036 <131113333+k22036@users.noreply.github.com>
2024-09-13 20:48:23 +0900

    fix: major,minor をinでcastするように

--- iBeacon.py
+++ iBeacon.py
@@ -8,8 +8,8 @@ bleno = Bleno()
 def iBeacon_start():
     # UUID (16バイト), Major (2バイト), Minor (2バイト), TX Power (1バイト)
     uuid = bytes.fromhex(config.iBeacon_uuid)
-    major = (config.iBeacon_major).to_bytes(2, byteorder='big')
-    minor = (config.iBeacon_minor).to_bytes(2, byteorder='big')
+    major = (int(config.iBeacon_major)).to_bytes(2, byteorder='big')
+    minor = (int(config.iBeacon_minor)).to_bytes(2, byteorder='big')
     measuredPower = -60
 
     bleno.stopAdvertising()
