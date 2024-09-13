092aacc2d73e50e481aa5c05e0c822fa1e5eb54d
k22036 <131113333+k22036@users.noreply.github.com>
2024-09-13 20:44:54 +0900

    change: iBeacon で別のインスタンスを使うように変更

--- iBeacon.py
+++ iBeacon.py
@@ -1,18 +1,8 @@
 from Config import Config
+from pybleno import Bleno
 
 config = Config()
-bleno = config.bleno
-
-
-# iBeaconの広告パケットの構成
-ibeacon_prefix = bytes([
-    0x02, 0x01, 0x1A,  # Flags
-    0x1A,  # Length of the remaining advertisement
-    0xFF,  # Manufacturer specific data
-    0x4C, 0x00,  # Apple company identifier (0x004C)
-    0x02,  # iBeacon type
-    0x15   # Length of remaining iBeacon data
-])
+bleno = Bleno()
 
 
 def iBeacon_start():
@@ -20,14 +10,9 @@ def iBeacon_start():
     uuid = bytes.fromhex(config.iBeacon_uuid)
     major = (config.iBeacon_major).to_bytes(2, byteorder='big')
     minor = (config.iBeacon_minor).to_bytes(2, byteorder='big')
-    # tx_power = (200).to_bytes(1, byteorder='big')
-
-    # 完全なiBeaconパケット
-    # ibeacon_packet = ibeacon_prefix + uuid + major + minor + tx_power
+    measuredPower = -60
 
     bleno.stopAdvertising()
 
-    # bleno.startAdvertisingWithEIRData(ibeacon_packet, bytes([]))
+    bleno.startAdvertisingIBeacon(uuid, major, minor, measuredPower)
     print('start iBeacon')
-
-    bleno.startAdvertisingIBeacon(uuid, major, minor, -55)
